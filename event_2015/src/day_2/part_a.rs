12b5331ea0fd590f893aeb959229bbfbb46f292f
= <jhowell45@googlemail.com>
2024-11-26 18:26:04 +0000

    Finished testing the function for getting the total surface area and slack of the present.

--- event_2015/src/day_2/part_a.rs
+++ event_2015/src/day_2/part_a.rs
@@ -1,5 +1,60 @@
 pub struct Present {
     length: i32,
     width: i32,
-    height: i32
+    height: i32,
+}
+
+impl Present {
+    pub fn new(length: i32, width: i32, height: i32) -> Self {
+        Self {
+            length,
+            width,
+            height,
+        }
+    }
+
+    pub fn surface_area(&self) -> i32 {
+        (2 * self.length_width_area())
+            + (2 * self.width_height_area())
+            + (2 * self.height_length_area()) + self.slack()
+    }
+
+    fn slack(&self) -> i32 {
+        *vec![
+            self.length_width_area(),
+            self.width_height_area(),
+            self.height_length_area(),
+        ]
+        .iter()
+        .min().unwrap()
+    }
+
+    fn length_width_area(&self) -> i32 {
+        self.length * self.width
+    }
+
+    fn width_height_area(&self) -> i32 {
+        self.width * self.height
+    }
+
+    fn height_length_area(&self) -> i32 {
+        self.height * self.length
+    }
+}
+
+pub fn part_a() {
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(2, 3, 4, 58)]
+    #[case(1, 1, 10, 43)]
+    fn examples(#[case] l: i32, #[case] w: i32, #[case] h: i32, #[case] area: i32) {
+        assert_eq!(Present::new(l, w, h).surface_area(), area)
+    }
 }
