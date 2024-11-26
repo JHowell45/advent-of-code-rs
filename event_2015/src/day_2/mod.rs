4642914714056403d8bb21dae634b6f8aa8939be
= <jhowell45@googlemail.com>
2024-11-26 18:44:32 +0000

    moving the present struct to a shared file as it will be used for part 2.

--- event_2015/src/day_2/mod.rs
+++ event_2015/src/day_2/mod.rs
@@ -1,2 +1,3 @@
+mod shared;
 pub mod part_a;
 pub mod part_b;
--- event_2015/src/day_2/part_a.rs
+++ event_2015/src/day_2/part_a.rs
@@ -1,57 +1,5 @@
 use core::{enums::Part, file_reader::get_file_contents};
-
-#[derive(Debug)]
-pub struct Present {
-    length: i32,
-    width: i32,
-    height: i32,
-}
-
-impl Present {
-    pub fn new(length: i32, width: i32, height: i32) -> Self {
-        Self {
-            length,
-            width,
-            height,
-        }
-    }
-
-    pub fn from_strings(length: &str, width: &str, height: &str) -> Self {
-        Self {
-            length: length.parse::<i32>().unwrap(),
-            width: width.parse::<i32>().unwrap(),
-            height: height.parse::<i32>().unwrap(),
-        }
-    }
-
-    pub fn surface_area(&self) -> i32 {
-        (2 * self.length_width_area())
-            + (2 * self.width_height_area())
-            + (2 * self.height_length_area()) + self.slack()
-    }
-
-    fn slack(&self) -> i32 {
-        *vec![
-            self.length_width_area(),
-            self.width_height_area(),
-            self.height_length_area(),
-        ]
-        .iter()
-        .min().unwrap()
-    }
-
-    fn length_width_area(&self) -> i32 {
-        self.length * self.width
-    }
-
-    fn width_height_area(&self) -> i32 {
-        self.width * self.height
-    }
-
-    fn height_length_area(&self) -> i32 {
-        self.height * self.length
-    }
-}
+use crate::day_2::shared::Present;
 
 pub fn part_a() {
     let mut total: i32 = 0;    
