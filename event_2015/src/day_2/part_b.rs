b432e1a0f65b76ec9864f7a2a585f02ae4d12695
= <jhowell45@googlemail.com>
2024-11-26 19:22:33 +0000

    finished part 2

--- event_2015/src/day_2/part_b.rs
+++ event_2015/src/day_2/part_b.rs
@@ -1,6 +1,16 @@
+use core::{enums::Part, file_reader::get_file_contents};
+
 use crate::day_2::shared::Present;
 
-pub fn part_b() {}
+pub fn part_b() {
+    let mut total: i32 = 0;    
+    for line in get_file_contents(2015, 2, Part::A).lines().into_iter() {
+        let [l, w, h]: [&str; 3] = line.split("x").collect::<Vec<&str>>().try_into().unwrap();
+        let present = Present::from_strings(l, w, h);
+        total += present.total_ribbon();
+    }
+    println!("Total ribbon: {total} square feet");
+}
 
 #[cfg(test)]
 mod tests {
