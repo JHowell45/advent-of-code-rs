97b120307af57b98a5107bd95bb5b8d6c15fb814
= <jhowell45@googlemail.com>
2024-11-27 21:03:19 +0000

    Creating tests for part B

--- event_2015/src/day_3/part_b.rs
+++ event_2015/src/day_3/part_b.rs
@@ -4,7 +4,8 @@ pub fn part_b() {}
 
 struct RoboSantaAndSantaLocations {
     santa: SantaLocation,
-    robo_santa: SantaLocation
+    robo_santa: SantaLocation,
+    swap_santa: bool
 }
 
 impl RoboSantaAndSantaLocations {
@@ -12,6 +13,13 @@ impl RoboSantaAndSantaLocations {
         Self {
             santa: SantaLocation::new(),
             robo_santa: SantaLocation::new(),
+            swap_santa: false
+        }
+    }
+
+    pub fn apply_directions(&mut self, directions: &str) {
+        for direction in directions.chars().into_iter() {
+
         }
     }
 }
@@ -21,6 +29,9 @@ mod tests {
     use super::*;
     use rstest::rstest;
 
-    #[test]
-    fn examples() {}
+    #[rstest]
+    #[case("^v", 3)]
+    #[case("^>v<", 3)]
+    #[case("^v^v^v^v^v", 11)]
+    fn examples(#[case] directions: &str, #[case] houses_visited: usize) {}
 }
