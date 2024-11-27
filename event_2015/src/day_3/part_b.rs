385f09bb70ddcf97a66c0f88ee34144111e2e952
= <jhowell45@googlemail.com>
2024-11-27 20:56:08 +0000

    Creating santas for the shared search.

--- event_2015/src/day_3/part_b.rs
+++ event_2015/src/day_3/part_b.rs
@@ -1,8 +1,20 @@
+use super::shared::SantaLocation;
+
 pub fn part_b() {}
 
-struct RoboSantaAndSantaLocations {}
+struct RoboSantaAndSantaLocations {
+    santa: SantaLocation,
+    robo_santa: SantaLocation
+}
 
-impl RoboSantaAndSantaLocations {}
+impl RoboSantaAndSantaLocations {
+    pub fn new() -> Self {
+        Self {
+            santa: SantaLocation::new(),
+            robo_santa: SantaLocation::new(),
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
