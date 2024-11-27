dd58a9b029f466ec7b6b0797e761be971ce8d57f
= <jhowell45@googlemail.com>
2024-11-27 21:12:18 +0000

    adding the swap directions for both santas.

--- event_2015/src/day_3/part_b.rs
+++ event_2015/src/day_3/part_b.rs
@@ -1,25 +1,32 @@
-use super::shared::SantaLocation;
+use std::collections::HashMap;
+
+use super::shared::{Location};
 
 pub fn part_b() {}
 
 struct RoboSantaAndSantaLocations {
-    santa: SantaLocation,
-    robo_santa: SantaLocation,
-    swap_santa: bool
+    santa: Location,
+    robo_santa: Location,
+    swap_santa: bool,
+    visited: HashMap<String, usize>
 }
 
 impl RoboSantaAndSantaLocations {
     pub fn new() -> Self {
         Self {
-            santa: SantaLocation::new(),
-            robo_santa: SantaLocation::new(),
-            swap_santa: false
+            santa: Location::new(),
+            robo_santa: Location::new(),
+            swap_santa: false,
+            visited: HashMap::new()
         }
     }
 
     pub fn apply_directions(&mut self, directions: &str) {
         for direction in directions.chars().into_iter() {
-
+            match self.swap_santa {
+                false => self.santa.add_direction(direction),
+                true => self.robo_santa.add_direction(direction)
+            }
         }
     }
 }
