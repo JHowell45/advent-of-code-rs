45552344991d535d6ee866b04bb3cd48546aabd2
= <jhowell45@googlemail.com>
2024-11-28 12:42:54 +0000

    Creating basic tests.

--- event_2015/src/day_4/part_a.rs
+++ event_2015/src/day_4/part_a.rs
@@ -1,1 +1,18 @@
 pub fn part_a() {}
+
+fn lowest_hex(secret: &str) -> usize {
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("abcdef", 609043)]
+    #[case("pqrstuv", 1048970)]
+    fn examples(#[case] secret: &str, #[case] answer: usize) {
+        assert_eq!(lowest_hex(secret), answer);
+    }
+}
