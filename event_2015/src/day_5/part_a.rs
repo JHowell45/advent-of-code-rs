b86a123e693f782d7b92b20ed9a1a774fe37f924
= <jhowell45@googlemail.com>
2024-11-29 12:03:08 +0000

    Creating shared stub strings and the basic function and tests.

--- event_2015/src/day_5/part_a.rs
+++ event_2015/src/day_5/part_a.rs
@@ -1,1 +1,19 @@
 pub fn part_a() {}
+
+fn is_nice_string(string: &str) -> bool {true}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("ugknbfddgicrmopn", true)]
+    #[case("aaa", true)]
+    #[case("jchzalrnumimnmhp", false)]
+    #[case("haegwjzuvuyypxyu", false)]
+    #[case("dvszwmarrgswjxmb", false)]
+    fn examples(#[case] string: &str, #[case] is_nice: bool) {
+        assert_eq!(is_nice_string(string), is_nice);
+    }
+}
---
+++ event_2015/src/day_5/shared.rs
@@ -0,0 +1,9 @@
+use std::collections::HashSet;
+
+pub static VOWELS: [&str; 5] = ["a", "e", "i", "o", "u"];
+pub static INVALID_STRINGS: [&str; 4] = [
+    "ab",
+    "cd",
+    "pq",
+    "xy"
+];
