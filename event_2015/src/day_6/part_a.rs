75a7c147c98e525e42deda5d09f6c56357041ccf
= <jhowell45@googlemail.com>
2024-11-29 16:26:52 +0000

    re-adding tests.

--- event_2015/src/day_6/part_a.rs
+++ event_2015/src/day_6/part_a.rs
@@ -35,22 +35,22 @@ mod tests {
     use super::*;
     use rstest::rstest;
 
-    // #[rstest]
-    // #[case("turn on 0,0 through 999,999", 1000000)]
-    // #[case("toggle 0,0 through 999,0", 1000)]
-    // fn example_instruction(#[case] instruction: &str, #[case] lights_on: usize) {
-    //     let mut formation = LightFormation::new();
-    //     instruction_parser(&formation, instruction);
-    //     assert_eq!(formation.number_of_lights_on(), lights_on);
-    // }
+    #[rstest]
+    #[case("turn on 0,0 through 999,999", 1000000)]
+    #[case("toggle 0,0 through 999,0", 1000)]
+    fn example_instruction(#[case] instruction: &str, #[case] lights_on: usize) {
+        let mut formation = LightFormation::new();
+        instruction_parser(&formation, instruction);
+        assert_eq!(formation.number_of_lights_on(), lights_on);
+    }
 
-    // #[rstest]
-    // #[case(vec!["turn on 0,0 through 999,999", "toggle 0,0 through 999,0", "turn off 499,499 through 500,500"], 4)]
-    // fn example_instructions(#[case] instructions: Vec<&str>, #[case] lights_on: usize) {
-    //     let mut formation = LightFormation::new();
-    //     for instruction in instructions.iter() {
-    //         instruction_parser(&formation, instruction);
-    //     }
-    //     assert_eq!(formation.number_of_lights_on(), lights_on);
-    // }
+    #[rstest]
+    #[case(vec!["turn on 0,0 through 999,999", "toggle 0,0 through 999,0", "turn off 499,499 through 500,500"], 4)]
+    fn example_instructions(#[case] instructions: Vec<&str>, #[case] lights_on: usize) {
+        let mut formation = LightFormation::new();
+        for instruction in instructions.iter() {
+            instruction_parser(&formation, instruction);
+        }
+        assert_eq!(formation.number_of_lights_on(), lights_on);
+    }
 }
