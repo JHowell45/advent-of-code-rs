d3ae52a05dff8fe8f778f1e4b200985aba4f365c
= <jhowell45@googlemail.com>
2024-11-29 16:15:28 +0000

    adding relevant tests before hand,

--- event_2015/src/day_6/part_a.rs
+++ event_2015/src/day_6/part_a.rs
@@ -1,6 +1,6 @@
 pub fn part_a() {}
 
-fn instruction_parser(instruction: &str) {}
+pub fn instruction_parser(formation: &LightFormation, instruction: &str) {}
 
 struct LightFormation {
     lights: [bool; 1000000]
@@ -12,6 +12,8 @@ impl LightFormation {
             lights: [false; 1000000]
         }
     }
+
+    pub fn number_of_lights_on(&self) -> usize {}
 
     pub fn toggle(&mut self) {}
 
@@ -26,7 +28,21 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    fn examples() {
+    #[case("turn on 0,0 through 999,999", 1000000)]
+    #[case("toggle 0,0 through 999,0", 1000)]
+    fn example_instruction(#[case] instruction: &str, #[case] lights_on: usize) {
+        let mut formation = LightFormation::new();
+        instruction_parser(&formation, instruction);
+        assert_eq!(formation.number_of_lights_on(), lights_on);
+    }
 
+    #[rstest]
+    #[case(vec!["turn on 0,0 through 999,999", "toggle 0,0 through 999,0", "turn off 499,499 through 500,500"], 4)]
+    fn example_instructions(#[case] instructions: Vec<&str>, #[case] lights_on: usize) {
+        let mut formation = LightFormation::new();
+        for instruction in instructions.iter() {
+            instruction_parser(&formation, instruction);
+        }
+        assert_eq!(formation.number_of_lights_on(), lights_on);
     }
 }
