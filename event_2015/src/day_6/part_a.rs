404119f52c89fb31af3a19643a2b25a72e506e1e
= <jhowell45@googlemail.com>
2024-11-29 22:46:14 +0000

    fixing tests.

--- event_2015/src/day_6/part_a.rs
+++ event_2015/src/day_6/part_a.rs
@@ -1,24 +1,44 @@
 pub fn part_a() {}
 
-pub fn instruction_parser(formation: &mut LightFormation, instruction: &str) {
+fn instruction_parser(formation: &mut LightFormation, instruction: &str) {
     let split: Vec<&str> = instruction.split(" ").collect();
     match split[0].to_lowercase().as_str() {
         "turn" => {
             let start = Coords::parse(split[2]);
             let finish = Coords::parse(split[4]);
+            println!("{:#?}", start);
+            println!("{:#?}", finish);
             match split[1].to_lowercase().as_str() {
                 "on" => {
-                    for y in start.y..finish.y {
-                        let start_index = start.x + (y * 1000);
-                        let finish_index = finish.x + (y * 1000);
+                    let mut y = start.y;
+                    let mut start_index = start.x + (y * 1000);
+                    let mut finish_index = finish.x + (y * 1000);
+                    formation.turn_on(start_index, finish_index);
+
+                    y += 1;
+                    
+                    while y <= finish.y {
+                        start_index = start.x + (y * 1000);
+                        finish_index = finish.x + (y * 1000);
                         formation.turn_on(start_index, finish_index);
+
+                        y += 1;
                     }
                 }
                 "off" => {
-                    for y in start.y..finish.y {
-                        let start_index = start.x + (y * 1000);
-                        let finish_index = finish.x + (y * 1000);
+                    let mut y = start.y;
+                    let mut start_index = start.x + (y * 1000);
+                    let mut finish_index = finish.x + (y * 1000);
+                    formation.turn_off(start_index, finish_index);
+
+                    y += 1;
+                    
+                    while y <= finish.y {
+                        start_index = start.x + (y * 1000);
+                        finish_index = finish.x + (y * 1000);
                         formation.turn_off(start_index, finish_index);
+
+                        y += 1;
                     }
                 }
                 _ => panic!("Unhandled starting sentence! '{instruction}'"),
@@ -27,10 +47,19 @@ pub fn instruction_parser(formation: &mut LightFormation, instruction: &str) {
         "toggle" => {
             let start = Coords::parse(split[1]);
             let finish = Coords::parse(split[3]);
-            for y in start.y..finish.y {
-                let start_index = start.x + (y * 1000);
-                let finish_index = finish.x + (y * 1000);
+            let mut y = start.y;
+            let mut start_index = start.x + (y * 1000);
+            let mut finish_index = finish.x + (y * 1000);
+            formation.toggle(start_index, finish_index);
+
+            y += 1;
+            
+            while y <= finish.y {
+                start_index = start.x + (y * 1000);
+                finish_index = finish.x + (y * 1000);
                 formation.toggle(start_index, finish_index);
+
+                y += 1;
             }
         }
         _ => panic!("Unhandled starting sentence! '{instruction}'"),
@@ -66,25 +95,25 @@ impl LightFormation {
     }
 
     pub fn number_of_lights_on(&self) -> usize {
-        self.lights.iter().filter(|l| **l == true).count()
+        self.lights.iter().filter(|&&l| l == true).count()
     }
 
     pub fn toggle(&mut self, start_index: usize, finish_index: usize) {
-        let _ = self.lights[start_index..finish_index]
-            .iter_mut()
-            .map(|l| *l = !(*l));
+        for i in start_index..=finish_index {
+            self.lights[i] = !self.lights[i];
+        }
     }
 
     pub fn turn_on(&mut self, start_index: usize, finish_index: usize) {
-        let _ = self.lights[start_index..finish_index]
-            .iter_mut()
-            .map(|l| *l = true);
+        for i in start_index..=finish_index {
+            self.lights[i] = true;
+        }
     }
 
     pub fn turn_off(&mut self, start_index: usize, finish_index: usize) {
-        let _ = self.lights[start_index..finish_index]
-            .iter_mut()
-            .map(|l| *l = false);
+        for i in start_index..=finish_index {
+            self.lights[i] = false;
+        }
     }
 }
 
