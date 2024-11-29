7c115e6052aae54ce25a3dc54d8aa22690dd769c
= <jhowell45@googlemail.com>
2024-11-29 13:45:16 +0000

    Adding final check for off-by-one identical letter.

--- event_2015/src/day_5/part_b.rs
+++ event_2015/src/day_5/part_b.rs
@@ -1,5 +1,5 @@
 use core::{enums::Part, file_reader::get_file_contents};
-use std::collections::{HashSet};
+use std::collections::HashSet;
 
 pub fn part_b() {
     let mut nice_strings: usize = 0;
@@ -18,6 +18,41 @@ fn is_nice_string(string: &str) -> bool {
 }
 
 #[derive(Debug)]
+struct LetterRepeat {
+    characters: [Option<char>; 3],
+}
+
+impl LetterRepeat {
+    pub fn new() -> Self {
+        Self {
+            characters: [None, None, None],
+        }
+    }
+
+    pub fn add(&mut self, letter: char) {
+        for i in 0..self.characters.len() {
+            let v: Option<char> = self.characters[i];
+            if v.is_none() {
+                self.characters[i] = Some(letter);
+                return;
+            }
+        }
+        self.rotate_and_add(letter);
+    }
+
+    fn rotate_and_add(&mut self, letter: char) {
+        for i in (self.characters.len() - 1)..0 {
+            self.characters[i - 1] = self.characters[i];
+        }
+        self.characters[2] = Some(letter);
+    }
+
+    pub fn check(&self) -> bool {
+        self.characters[0] == self.characters[2]
+    }
+}
+
+#[derive(Debug)]
 struct NiceString {
     overlapping_pairs: bool,
     pair_check: bool,
@@ -31,6 +66,7 @@ impl NiceString {
         let mut spaced_letter_repeat: bool = false;
 
         let mut pairs_lookup: HashSet<String> = HashSet::new();
+        let mut letter_repeat: LetterRepeat = LetterRepeat::new();
 
         let mut prior: Option<char> = None;
         let mut prior_pair: String = String::from("");
@@ -42,12 +78,21 @@ impl NiceString {
                     overlapping_pairs = true;
                     break;
                 }
+
                 if !pair_check {
                     match pairs_lookup.contains(&pair) {
                         true => pair_check = true,
-                        false => {pairs_lookup.insert(pair.clone());},
+                        false => {
+                            pairs_lookup.insert(pair.clone());
+                        }
                     }
                 }
+
+                if !spaced_letter_repeat {
+                    letter_repeat.add(letter);
+                    spaced_letter_repeat = letter_repeat.check();
+                }
+
                 prior_pair = pair;
             }
             prior = Some(letter);
