50e27411f3f499fa3ae213db343555ac7db057a5
= <jhowell45@googlemail.com>
2024-11-29 12:30:47 +0000

    Finished part a and got the correct answer.

--- event_2015/src/day_5/part_a.rs
+++ event_2015/src/day_5/part_a.rs
@@ -1,10 +1,19 @@
+use core::{enums::Part, file_reader::get_file_contents};
+
 use super::shared::NiceString;
 
-pub fn part_a() {}
+pub fn part_a() {
+    let mut nice_strings: usize = 0;
+    for string in get_file_contents(2015, 5, Part::A).lines().into_iter() {
+        if is_nice_string(string) {
+            nice_strings += 1;
+        }
+    }
+    println!("The number of nice strings: {}", nice_strings);
+}
 
 fn is_nice_string(string: &str) -> bool {
     let nice = NiceString::parse(string);
-    println!("{:#?}", &nice);
     nice.result()
 }
 
