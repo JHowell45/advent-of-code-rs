d923359f94f4a73189b405ea7deeb9fffe633365
= <jhowell45@googlemail.com>
2024-11-29 16:09:10 +0000

    basic struct for handling the lights, and stub parser for the instructions.

--- event_2015/src/day_6/part_a.rs
+++ event_2015/src/day_6/part_a.rs
@@ -1,1 +1,26 @@
 pub fn part_a() {}
+
+fn instruction_parser(instruction: &str) {}
+
+struct LightFormation {
+    lights: [bool; 1000000]
+}
+
+impl LightFormation {
+    pub fn new() -> Self {
+        Self {
+            lights: [false; 1000000]
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    fn examples() {
+
+    }
+}
