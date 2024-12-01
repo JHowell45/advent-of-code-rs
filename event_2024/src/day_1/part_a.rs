056023818c12bbccbe65fe0876da3af601a86153
= <jhowell45@googlemail.com>
2024-12-01 14:46:20 +0000

    got part A working.

--- event_2024/src/day_1/part_a.rs
+++ event_2024/src/day_1/part_a.rs
@@ -11,7 +11,7 @@ pub fn part_a() {
 }
 
 fn parse_line(line: &str) -> (i32, i32) {
-    let inputs: Vec<&str> = line.split(" ").collect();
+    let inputs: Vec<&str> = line.split(" ").filter(|v| *v != "").collect();
     (inputs[0].trim().parse::<i32>().unwrap(), inputs[1].trim().parse::<i32>().unwrap())
 }
 
@@ -51,4 +51,16 @@ impl LocationIds {
 mod tests {
     use super::*;
     use rstest::rstest;
+
+    #[rstest]
+    #[case("3   4\n4   3\n2   5\n1   3\n3   9\n3   3", 11)]
+    fn example(#[case] locations: &str, #[case] distance: usize) {
+        let mut search = LocationIds::new();
+        for (index, line) in locations.lines().into_iter().enumerate() {
+            let (left, right) = parse_line(line);
+            search.add_left(index, left);
+            search.add_right(index, right);
+        }
+        assert_eq!(search.total_distances(), distance);
+    }
 }
