b0bfc01c35aa5367b3c39674682b2a7127432cad
= <jhowell45@googlemail.com>
2024-12-02 18:37:19 +0000

    formatting.

--- event_2024/src/day_1/part_b.rs
+++ event_2024/src/day_1/part_b.rs
@@ -15,6 +15,9 @@ mod tests {
     #[rstest]
     #[case("3   4\n4   3\n2   5\n1   3\n3   9\n3   3", 31)]
     fn example(#[case] locations: &str, #[case] similarity: usize) {
-        assert_eq!(LocationSearch::parse_input(locations).similarity_score(), similarity);
+        assert_eq!(
+            LocationSearch::parse_input(locations).similarity_score(),
+            similarity
+        );
     }
 }
