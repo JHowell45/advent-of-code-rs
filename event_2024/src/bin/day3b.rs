0eb0b39000a692cbe477db72546f0ee314a055c5
= <jhowell45@googlemail.com>
2024-12-03 19:52:04 +0000

    part B cleanup

--- event_2024/src/bin/day3b.rs
+++ event_2024/src/bin/day3b.rs
@@ -3,20 +3,22 @@ use regex::Regex;
 
 
 fn main() {
-    // let mut total: i32 = 0;
     let puzzle_data: String = get_file_contents(2024, 3);
-    let pattern = Regex::new("(mul\\((?<a>\\d{1,3}),(?<b>\\d{1,3})\\))").unwrap();
-    // for (a, b) in pattern.captures_iter(&puzzle_data).map(|caps| {
-    //     let a = caps.name("a").unwrap().as_str().parse::<i32>().unwrap();
-    //     let b = caps.name("b").unwrap().as_str().parse::<i32>().unwrap();
-    //     (a, b)
-    // }) {
-    //     total += a * b;
-    // }
-    let total: i32 = pattern.captures_iter(&puzzle_data).map(|caps| {
-        let a = caps.name("a").unwrap().as_str().parse::<i32>().unwrap();
-        let b = caps.name("b").unwrap().as_str().parse::<i32>().unwrap();
-        a * b
-    }).sum();
-    println!("Sum of enabled multiplications: {total:}");
+    println!("Sum of enabled multiplications: {}", enabled_sum(&puzzle_data));
+}
+
+fn enabled_sum(text: &str) -> i32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", 48)]
+    fn example(#[case] text: &str, #[case] sum: i32) {
+        assert_eq!(enabled_sum(text), sum);
+    }
 }
