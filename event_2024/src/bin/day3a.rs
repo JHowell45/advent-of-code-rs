e7d2384a05ad3e339e52c487aab12f853b5a78fc
= <jhowell45@googlemail.com>
2024-12-03 19:15:05 +0000

    speed up using groups

--- event_2024/src/bin/day3a.rs
+++ event_2024/src/bin/day3a.rs
@@ -5,11 +5,13 @@ use regex::Regex;
 fn main() {
     let mut total: i32 = 0;
     let puzzle_data: String = get_file_contents(2024, 3);
-    let pattern = Regex::new("(mul\\(\\d{1,3},\\d{1,3}\\))").unwrap();
-    for data in pattern.find_iter(&puzzle_data).map(|c| c.as_str()) {
-        let m = Regex::new("\\d{1,3},\\d{1,3}").unwrap().find(data).unwrap();
-        let numbers: Vec<i32> = m.as_str().split(",").map(|n| n.trim().parse::<i32>().unwrap()).collect();
-        total += numbers[0] * numbers[1];
+    let pattern = Regex::new("(mul\\((?<a>\\d{1,3}),(?<b>\\d{1,3})\\))").unwrap();
+    for (a, b) in pattern.captures_iter(&puzzle_data).map(|caps| {
+        let a = caps.name("a").unwrap().as_str().parse::<i32>().unwrap();
+        let b = caps.name("b").unwrap().as_str().parse::<i32>().unwrap();
+        (a, b)
+    }) {
+        total += a * b;
     }
     println!("Sum of multiplications: {total:}");
 }
