02b53dd97929cac8c4bed5a7aab4018d1713ba54
= <jhowell45@googlemail.com>
2024-12-03 19:01:04 +0000

    finished part A, but it's very slow

--- event_2024/src/bin/day3a.rs
+++ event_2024/src/bin/day3a.rs
@@ -3,11 +3,13 @@ use regex::Regex;
 
 
 fn main() {
-    let mut total: usize = 0;
+    let mut total: i32 = 0;
     let puzzle_data: String = get_file_contents(2024, 3);
     let pattern = Regex::new("(mul\\(\\d{1,3},\\d{1,3}\\))").unwrap();
-    for (_, [path, lineno, line]) in pattern.captures_iter(&puzzle_data).map(|c| c.extract()) {
-        println!("{path:} | {lineno:} | {line:}");
+    for data in pattern.find_iter(&puzzle_data).map(|c| c.as_str()) {
+        let m = Regex::new("\\d{1,3},\\d{1,3}").unwrap().find(data).unwrap();
+        let numbers: Vec<i32> = m.as_str().split(",").map(|n| n.trim().parse::<i32>().unwrap()).collect();
+        total += numbers[0] * numbers[1];
     }
     println!("Sum of multiplications: {total:}");
 }
