c8566a11c32c0a3206c834d81a4e6d7fd2b2952d
= <jhowell45@googlemail.com>
2024-12-04 23:04:33 +0000

    fixing a test.

--- event_2024/src/bin/day4a.rs
+++ event_2024/src/bin/day4a.rs
@@ -81,7 +81,7 @@ impl WordSearch {
         return None;
     }
 
-    pub fn right(&self, index: usize, word_length: usize) -> Option<String> {
+    fn right(&self, index: usize, word_length: usize) -> Option<String> {
         if index % self.columns < self.columns - word_length + 1 {
             let local_word: String = self.letters[index..index + word_length].iter().collect();
             return Some(local_word);
