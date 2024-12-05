42422fd2eb027b7ba9172f803d244b18326f5b6e
= <jhowell45@googlemail.com>
2024-12-05 19:01:45 +0000

    creating a map of the next values to check and iterate over.

--- event_2024/src/bin/day5a.rs
+++ event_2024/src/bin/day5a.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 fn main() {}
 
 struct LaunchSafetyManual {
@@ -10,18 +12,48 @@ impl LaunchSafetyManual {
         let split: Vec<&str> = text.split("\n\n").collect();
         let [page_ordering_rules, page_numbers] = [split[0], split[1]];
         Self {
-            rules: PageOrderingRules::new(),
+            rules: PageOrderingRules::from_string(page_ordering_rules),
             page_numbers: page_numbers.split(',').map(|n| n.parse::<i32>().unwrap()).collect()
         }
     }
 }
 
-struct PageOrderingRules {}
+struct PageOrderingRules {
+    values_after: HashMap<i32, HashSet<i32>>
+}
 
 impl PageOrderingRules {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            values_after: HashMap::new()
+        }
     }
+
+    pub fn from_string(rules: &str) -> Self {
+        let mut instance = Self::new();
+        for rule in rules.lines().into_iter() {
+            let split: Vec<&str> = rule.split("|").collect();
+            let (a, b) = (split[0].parse::<i32>().unwrap(), split[0].parse::<i32>().unwrap());
+            instance.add_rule(a, b);
+        }
+        return instance;
+    }
+
+    pub fn add_rule(&mut self, key: i32, value: i32) {
+        match self.values_after.get_mut(key) {
+            Some(valid) => {
+                valid.insert(value);
+            }
+            None => {
+                self.values_after.insert(key, HashSet::from([value]));
+            }
+        }
+    }
+
+    pub fn validate_next(&self, current: i32, next: i32) -> bool {
+        self.values_after.contains_key(&current) || self.values_after.get(&current).unwrap().contains(&next)
+    }
+
 }
 
 #[cfg(test)]
