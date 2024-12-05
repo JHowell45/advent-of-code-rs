5105026829bc342e011d7472aacf0255862beca8
= <jhowell45@googlemail.com>
2024-12-05 14:04:39 +0000

    setting up the root model.

--- event_2024/src/bin/day5a.rs
+++ event_2024/src/bin/day5a.rs
@@ -1,12 +1,18 @@
 fn main() {}
 
-struct LaunchSafetyManual {}
+struct LaunchSafetyManual {
+    rules: PageOrderingRules,
+    page_numbers: Vec<i32>
+}
 
 impl LaunchSafetyManual {
     pub fn from_string(text: &str) -> Self {
         let split: Vec<&str> = text.split("\n\n").collect();
         let [page_ordering_rules, page_numbers] = [split[0], split[1]];
-        Self {}
+        Self {
+            rules: PageOrderingRules::new(),
+            page_numbers: page_numbers.split(',').map(|n| n.parse::<i32>().unwrap()).collect()
+        }
     }
 }
 
