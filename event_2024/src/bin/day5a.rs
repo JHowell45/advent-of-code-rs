aefba273eb93a26ca314660718046c25b37724cd
= <jhowell45@googlemail.com>
2024-12-05 20:02:21 +0000

    hide print.

--- event_2024/src/bin/day5a.rs
+++ event_2024/src/bin/day5a.rs
@@ -4,7 +4,7 @@ use std::collections::{HashMap, HashSet};
 fn main() {
     let content = get_file_contents(2024, 5);
     let manual = LaunchSafetyManual::from_string(&content);
-    println!("{manual:?}");
+    // println!("{manual:?}");
     println!("Sum of middle pages: {}", manual.sum_middle_values());
 }
 
