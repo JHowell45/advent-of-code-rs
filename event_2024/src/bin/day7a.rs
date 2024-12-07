d65a346d271234caaef6b478bd31c9507fbeb9cf
Jacob <jacob@notactivelylooking.com>
2024-12-07 20:41:58 +0000

    adding parsing for the equations.

--- event_2024/src/bin/day7a.rs
+++ event_2024/src/bin/day7a.rs
@@ -1,39 +1,55 @@
+use itertools::Itertools;
+
 fn main() {}
 
 #[derive(Debug, Clone, Copy)]
 enum Operator {
-    Plus,
-    Multiply
+    Add,
+    Multiply,
 }
 
 fn sum_of_valid_results(text: &str) -> i32 {
-    text.lines().map(|equation| {
-        let (result, numbers) = parse_input(equation);
-        if validate_equation(result, numbers) {
-            return result;
-        }
-        return 0;
-    }).sum()
+    text.lines()
+        .map(|equation| {
+            let (result, numbers) = parse_input(equation);
+            if validate_equation(result, numbers) {
+                return result;
+            }
+            return 0;
+        })
+        .sum()
 }
 
 fn parse_input(input: &str) -> (i32, Vec<i32>) {
     let split: Vec<&str> = input.split(": ").collect();
     (
         split[0].parse::<i32>().unwrap(),
-        split[1].split(" ").map(|n| n.parse::<i32>().unwrap()).collect()
+        split[1]
+            .split(" ")
+            .map(|n| n.parse::<i32>().unwrap())
+            .collect(),
     )
 }
 
 fn validate_equation(result: i32, numbers: Vec<i32>) -> bool {
-    let mut operators = vec![Operator::Plus; numbers.len() - 1];
+    for operators in [Operator::Add, Operator::Multiply]
+        .iter()
+        .combinations(numbers.len() - 1)
+    {
+        if validate_equation_ops(result, &numbers, &operators) {
+            return true;
+        }
+    }
+    return false;
 }
 
-fn evaluate_equation_ops(result: i32, numbers: Vec<i32>, operators: Vec<Operator>) -> bool {
+fn validate_equation_ops(result: i32, numbers: &Vec<i32>, operators: &Vec<&Operator>) -> bool {
     let mut total = numbers[0];
+    println!("{operators:?}");
     for idx in 1..numbers.len() {
         let v = numbers[idx];
         match operators[idx - 1] {
-            Operator::Plus => total += v,
+            Operator::Add => total += v,
             Operator::Multiply => total *= v,
         }
         if total > result {
@@ -49,7 +65,8 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    #[case("190: 10 19
+    #[case(
+        "190: 10 19
 3267: 81 40 27
 83: 17 5
 156: 15 6
@@ -57,7 +74,9 @@ mod tests {
 161011: 16 10 13
 192: 17 8 14
 21037: 9 7 18 13
-292: 11 6 16 20", 3749)]
+292: 11 6 16 20",
+        3749
+    )]
     fn example(#[case] data: &str, #[case] expected: i32) {
         assert_eq!(sum_of_valid_results(data), expected);
     }
