818fca1a1e2bb3e8e894c8ee29824d62bc5917d3
Jacob <jacob@notactivelylooking.com>
2024-12-07 19:38:53 +0000

    Adding parsing function for the strings.

--- event_2024/src/bin/day7a.rs
+++ event_2024/src/bin/day7a.rs
@@ -1,5 +1,20 @@
 fn main() {}
 
+enum Operator {
+    Plus,
+    Multiply
+}
+
+fn parse_input(input: &str) -> (i32, Vec<i32>) {
+    let split: Vec<&str> = input.split(": ").collect();
+    (
+        split[0].parse::<i32>().unwrap(),
+        split[1].split(" ").map(|n| n.parse::<i32>().unwrap()).collect()
+    )
+}
+
+fn evaluate_function(result: i32, numbers: Vec<i32>, ops: Vec<i32>) -> bool {}
+
 #[cfg(test)]
 mod tests {
     use super::*;
