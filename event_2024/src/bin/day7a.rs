51ee01895d46ec096d79e6463dee66e1ac21d216
Jacob <jacob@notactivelylooking.com>
2024-12-07 19:44:26 +0000

    function for getting the sum of valid results for functions.

--- event_2024/src/bin/day7a.rs
+++ event_2024/src/bin/day7a.rs
@@ -5,6 +5,16 @@ enum Operator {
     Multiply
 }
 
+fn sum_of_valid_results(text: &str) -> i32 {
+    text.lines().map(|equation| {
+        let (result, numbers) = parse_input(equation);
+        if evaluate_function(result, numbers) {
+            return result;
+        }
+        return 0;
+    }).sum()
+}
+
 fn parse_input(input: &str) -> (i32, Vec<i32>) {
     let split: Vec<&str> = input.split(": ").collect();
     (
@@ -13,7 +23,7 @@ fn parse_input(input: &str) -> (i32, Vec<i32>) {
     )
 }
 
-fn evaluate_function(result: i32, numbers: Vec<i32>, ops: Vec<i32>) -> bool {}
+fn evaluate_function(result: i32, numbers: Vec<i32>) -> bool {}
 
 #[cfg(test)]
 mod tests {
@@ -21,5 +31,16 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    fn example() {}
+    #[case("190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20", 3749)]
+    fn example(#[case] data: &str, #[case] expected: i32) {
+        assert_eq!(sum_of_valid_results(data), expected);
+    }
 }
