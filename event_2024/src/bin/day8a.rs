06bacf27c54ef9b74999373d37f48a7c2b97dec6
Jacob <jacob@notactivelylooking.com>
2024-12-08 17:11:14 +0000

    adding more class methods for handling diff input types.

--- event_2024/src/bin/day8a.rs
+++ event_2024/src/bin/day8a.rs
@@ -9,7 +9,7 @@ fn main() {
     println!("Unique antinode locations: {}", map.unique_antinode_locations());
 }
 
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 struct Point {
     pub x: i32,
     pub y: i32
@@ -20,6 +20,10 @@ impl Point {
         Self {
             x, y
         }
+    }
+
+    pub fn from_usize(x: usize, y: usize) -> Self {
+        Self::new(x as i32, y as i32)
     }
 }
 
@@ -47,14 +51,14 @@ impl Sub for Point {
 
 
 struct FrequencyMap {
-    antennna_locations: HashMap<char, HashSet<(i32, i32)>>,
-    y_dimension: usize,
+    antennna_locations: HashMap<char, HashSet<Point>>,
+    x_dimension: usize,
     y_dimension: usize,
 }
 
 impl FrequencyMap {
     pub fn from_map(map: &str) -> Self {
-        let mut antennna_locations: HashMap<char, HashSet<(i32, i32)>> = HashMap::new();
+        let mut antennna_locations: HashMap<char, HashSet<Point>> = HashMap::new();
         let mut x: usize = 0;
         let mut y: usize = 0;
         for (y_idx, row) in map.lines().into_iter().enumerate() {
@@ -62,7 +66,7 @@ impl FrequencyMap {
                 x = row.chars().collect::<Vec<char>>().len();
             }
             for (idx, node) in row.chars().into_iter().enumerate() {
-                let point = (idx as i32, y_idx as i32);
+                let point = Point::from_usize(idx, y_idx);
                 match node {
                     '.' => {}
                     _ => match antennna_locations.get_mut(&node) {
@@ -85,7 +89,7 @@ impl FrequencyMap {
     }
 
     pub fn unique_antinode_locations(&self) -> usize {
-        let antinode_locations: HashSet<(i32, i32)> = HashSet::new();
+        let antinode_locations: HashSet<Point> = HashSet::new();
         for points in self.antennna_locations.values() {
             for x in repeat_n(points.iter(), 2).multi_cartesian_product() {
                 let (a, b) = (x[0], x[1]);
@@ -97,17 +101,14 @@ impl FrequencyMap {
         return antinode_locations.len();
     }
 
-    fn calculate_distance(&self, from: (i32, i32), to: (i32, i32)) -> (i32, i32) {
-        let (from_x, from_y) = from;
-        let (to_x, to_y) = to;
-        ((to_x - from_x), (to_y - from_y))
+    fn calculate_distance(&self, from: Point, to: Point) -> Point {
+        to - from
     }
 
     pub fn display_map(&self) {
         for y in 0..self.y_dimension {
             for x in 0..self.x_dimension {
-                let point: (i32, i32) = (x as i32, y as i32);
-                match self.slow_antenna_check(point) {
+                match self.slow_antenna_check(Point::from_usize(x, y)) {
                     Some(c) => print!("{c:}"),
                     None => print!("."),
                 }
@@ -117,7 +118,7 @@ impl FrequencyMap {
         println!();
     }
 
-    fn slow_antenna_check(&self, point: (i32, i32)) -> Option<char> {
+    fn slow_antenna_check(&self, point: Point) -> Option<char> {
         for (k, points) in self.antennna_locations.iter() {
             if let Some(_) = points.get(&point) {
                 return Some(*k);
