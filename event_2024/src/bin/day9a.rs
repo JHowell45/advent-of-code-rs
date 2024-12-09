f781c5bfdb538c0255da344737f9635acf56db78
Jacob <jacob@notactivelylooking.com>
2024-12-09 13:36:48 +0000

    correct iterator to get the relevant data.

--- event_2024/src/bin/day9a.rs
+++ event_2024/src/bin/day9a.rs
@@ -1,4 +1,44 @@
+use itertools::Itertools;
+
 fn main() {}
+
+#[derive(Debug)]
+pub struct FileMap {
+    id: usize,
+    blocks: usize,
+    free: usize,
+}
+
+impl FileMap {
+    pub fn new(id: usize, blocks: usize, free: usize) -> Self {
+        Self { id, blocks, free }
+    }
+}
+
+#[derive(Debug)]
+pub struct DiskMap {
+    files: Vec<FileMap>,
+}
+
+impl DiskMap {
+    pub fn from_map(map: &str) -> Self {
+        for (blocks, free) in map.chars().chunks(2).into_iter().map(|mut chunk| {
+            (
+                match chunk.next() {
+                    Some(n) => n.to_string().parse::<usize>().unwrap(),
+                    None => 0,
+                },
+                match chunk.next() {
+                    Some(n) => n.to_string().parse::<usize>().unwrap(),
+                    None => 0,
+                },
+            )
+        }) {
+            println!("{blocks:?}, {free:?}");
+        }
+        Self { files: Vec::new() }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -6,5 +46,9 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    fn example() {}
+    #[case("2333133121414131402")]
+    fn example(#[case] disk_map: &str) {
+        let map = DiskMap::from_map(disk_map);
+        println!("{map:?}");
+    }
 }
