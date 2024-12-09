626e359afac7d26726b0b1292ebc9de959ea423b
Jacob <jacob@notactivelylooking.com>
2024-12-09 18:03:25 +0000

    adding part b test and stub

--- event_2024/src/bin/day9b.rs
+++ event_2024/src/bin/day9b.rs
@@ -1,4 +1,12 @@
-fn main() {}
+use core::file_reader::get_file_contents;
+
+use event_2024::shared::day9::DiskMap;
+
+fn main() {
+    let mut disk = DiskMap::from_map(get_file_contents(2024, 9).as_str());
+    disk.defragment();
+    println!("Filesystem Checksum: {}", disk.checksum());
+}
 
 #[cfg(test)]
 mod tests {
@@ -6,5 +14,10 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    fn example() {}
+    #[case("2333133121414131402", 2858)]
+    fn example(#[case] disk_map: &str, #[case] expected_checksum: usize) {
+        let mut map = DiskMap::from_map(disk_map);
+        map.defragment();
+        assert_eq!(map.checksum(), expected_checksum);
+    }
 }
