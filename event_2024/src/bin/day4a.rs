470c568668a349d75ff5e7a1ddeb8f3a11cc3c08
Jacob <jacob@notactivelylooking.com>
2024-12-11 21:06:00 +0000

    got part a working.

--- event_2024/src/bin/day4a.rs
+++ event_2024/src/bin/day4a.rs
@@ -3,8 +3,10 @@ use core::file_reader::get_file_contents;
 use event_2024::shared::day4::WordSearch;
 
 fn main() {
+    let word: &str = "XMAS";
     let word_search = WordSearch::from_string(get_file_contents(2024, 4).as_str());
     word_search.display_search();
+    println!("Number of times {word:} appears: {}", word_search.search(word));
 }
 
 #[cfg(test)]
