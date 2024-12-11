9efa5317b8ab3d1f2a38ab469ecb597fe5a9ca79
Jacob <jacob@notactivelylooking.com>
2024-12-11 12:57:37 +0000

    create stones from the provided string.

--- event_2024/src/shared/day11.rs
+++ event_2024/src/shared/day11.rs
@@ -1,3 +1,16 @@
 pub struct Stones {
-    
+    pub stones: Vec<i32>,
+}
+
+impl Stones {
+    pub fn from_string(text: &str) -> Self {
+        Self {
+            stones: text
+                .split(" ")
+                .map(|s| s.to_string().parse::<i32>().unwrap())
+                .collect(),
+        }
+    }
+
+    pub fn blink(&mut self) {}
 }
