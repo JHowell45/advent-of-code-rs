b68f05d8d6b043c843ec8948886c3e16a503d916
Jacob <jacob@notactivelylooking.com>
2024-12-11 13:08:22 +0000

    Finished blink method.

--- event_2024/src/shared/day11.rs
+++ event_2024/src/shared/day11.rs
@@ -1,23 +1,28 @@
 pub struct Stones {
-    pub stones: Vec<i32>,
+    pub stones: Vec<String>,
 }
 
 impl Stones {
     pub fn from_string(text: &str) -> Self {
         Self {
-            stones: text
-                .split(" ")
-                .map(|s| s.to_string().parse::<i32>().unwrap())
-                .collect(),
+            stones: text.split(" ").map(|s| s.to_string()).collect(),
         }
     }
 
     pub fn blink(&mut self) {
-        for stone in self.stones.iter_mut() {
-            if *stone == 0 {
-                *stone = 1;
+        let mut new_stones: Vec<String> = Vec::new();
+        for stone in self.stones.iter() {
+            let stone_v = stone.parse::<i32>().unwrap();
+            if stone_v == 0 {
+                new_stones.push(1.to_string());
+            } else if stone.len() % 2 == 0 {
+                new_stones.push(stone[0..stone.len() / 2].to_string());
+                new_stones.push(stone[stone.len() / 2..stone.len()].to_string());
+            } else {
+                new_stones.push((stone_v * 2024).to_string().to_string());
             }
         }
+        self.stones = new_stones;
     }
 }
 
@@ -27,7 +32,5 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    fn test_blink() {
-
-    }
+    fn test_blink() {}
 }
