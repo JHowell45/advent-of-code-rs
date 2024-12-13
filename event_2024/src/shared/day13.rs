c64e3f67c2122729e619d5aaa348d204c05e819a
Jacob <jacob@notactivelylooking.com>
2024-12-13 19:53:35 +0000

    more basic add/sub traits

--- event_2024/src/shared/day13.rs
+++ event_2024/src/shared/day13.rs
@@ -1,4 +1,4 @@
-use std::ops::Mul;
+use std::ops::{Add, Mul, Sub};
 
 pub struct Point {
     x: usize,
@@ -16,5 +16,21 @@ impl Mul<usize> for Point {
 
     fn mul(self, rhs: usize) -> Self::Output {
         Self::new(self.x * rhs, self.y * rhs)
+    }
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(self.x + rhs.x, self.y + rhs.y)
+    }
+}
+
+impl Sub for Point {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Self::new(self.x - rhs.x, self.y - rhs.y)
     }
 }
