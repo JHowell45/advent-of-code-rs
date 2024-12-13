5088655b683e5401e81866a93050506242749f1a
Jacob <jacob@notactivelylooking.com>
2024-12-13 19:52:28 +0000

    point and basic multiply impl for it

---
+++ event_2024/src/shared/day13.rs
@@ -0,0 +1,20 @@
+use std::ops::Mul;
+
+pub struct Point {
+    x: usize,
+    y: usize,
+}
+
+impl Point {
+    pub fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
+}
+
+impl Mul<usize> for Point {
+    type Output = Self;
+
+    fn mul(self, rhs: usize) -> Self::Output {
+        Self::new(self.x * rhs, self.y * rhs)
+    }
+}
