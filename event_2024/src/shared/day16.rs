520bea3ce3fe88ba1ffbb77a733db5c1c845eddf
= <jhowell45@googlemail.com>
2024-12-16 23:54:53 +0000

    creating shared file stubs

---
+++ event_2024/src/shared/day16.rs
@@ -0,0 +1,24 @@
+pub struct Map {
+
+}
+
+impl Map {
+    pub fn from_string(map: &str) -> Self {
+        Self {
+
+        }
+    }
+}
+
+pub struct PathFinder {
+
+}
+
+impl PathFinder {
+
+}
+
+#[cfg(test)]
+mod tests {
+    
+}
