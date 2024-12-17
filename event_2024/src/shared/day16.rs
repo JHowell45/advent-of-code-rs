529ac21efbe640ed1d97a0b8d638b3ef323c81d9
= <jhowell45@googlemail.com>
2024-12-17 00:01:22 +0000

    creating basic pathfinder

--- event_2024/src/shared/day16.rs
+++ event_2024/src/shared/day16.rs
@@ -1,5 +1,20 @@
-pub struct Map {
+pub struct Point {
+    x: usize,
+    y: usize,
+}
 
+impl Point {
+    pub fn new(x: usize, y: usize) -> Self {
+        Self { x, y }
+    }
+}
+
+pub struct Map {
+    data: Vec<char>,
+    y_size: usize,
+    x_size: usize,
+    start: Point,
+    end: Point
 }
 
 impl Map {
@@ -11,14 +26,21 @@ impl Map {
 }
 
 pub struct PathFinder {
-
+    map: Map,
+    score: usize
 }
 
 impl PathFinder {
+    pub fn from_string(map: &str) -> Self {
 
+    }
+
+    fn move_forward(&mut self) {}
+
+    fn rotate(&mut self) {}
 }
 
 #[cfg(test)]
 mod tests {
-    
+
 }
