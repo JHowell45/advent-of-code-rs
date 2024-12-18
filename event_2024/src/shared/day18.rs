72cb9df46500242c40c5aae891cb908114b4214c
= <jhowell45@googlemail.com>
2024-12-18 18:47:05 +0000

    basic impl

---
+++ event_2024/src/shared/day18.rs
@@ -0,0 +1,34 @@
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Point {
+    x: u8,
+    y: u8
+}
+
+impl Point {
+    pub fn new(x: u8, y: u8) -> Self {
+        Self { x, y }
+    }
+}
+
+pub struct MemorySpace {
+    space: usize,
+    bytes: Vec<Point>
+}
+
+impl MemorySpace {
+    pub fn new(range: usize, bytes: Vec<Point>) -> Self {
+        Self {
+            space: range,
+            bytes: bytes
+        }
+    }
+
+    pub fn display_space(&self) {
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+}
