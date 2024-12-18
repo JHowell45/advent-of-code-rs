2c8b84900236495fdde1e89366cfa3818b582fad
= <jhowell45@googlemail.com>
2024-12-18 19:35:38 +0000

    got part 1 right

--- event_2024/src/bin/day18a.rs
+++ event_2024/src/bin/day18a.rs
@@ -1,4 +1,11 @@
-fn main() {}
+use core::file_reader::get_file_contents;
+
+use event_2024::shared::day18::MemorySpace;
+
+fn main() {
+    let mut space = MemorySpace::from_string(70, get_file_contents(2024, 18).as_str());
+    println!("Least number of steps: {}", space.least_steps(1024));
+}
 
 #[cfg(test)]
 mod tests {
