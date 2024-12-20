e1e808e8b62807e275120eeb98883f96d54bef18
= <jhowell45@googlemail.com>
2024-12-20 00:15:45 +0000

    adding day a

--- event_2024/src/bin/day19a.rs
+++ event_2024/src/bin/day19a.rs
@@ -1,4 +1,8 @@
-fn main() {}
+use core::file_reader::get_file_contents;
+
+fn main() {
+    let (towels, racks) = get_file_contents(2024, 19).split("\n\n").collect::<[Vec<String>; 2]>();
+}
 
 #[cfg(test)]
 mod tests {
