049a96e931d72d8df29e15011811a2cca73ddd0c
= <jhowell45@googlemail.com>
2025-12-03 20:22:21 +0000

    adding more tests.

--- event_2025/src/bin/day2b.rs
+++ event_2025/src/bin/day2b.rs
@@ -43,21 +43,21 @@ mod tests {
     use super::*;
     use rstest::rstest;
 
-    // #[rstest]
-    // #[case("11", true)]
-    // #[case("12", false)]
-    // #[case("13", false)]
-    // #[case("14", false)]
-    // #[case("15", false)]
-    // #[case("16", false)]
-    // #[case("22", true)]
-    // #[case("1010", true)]
-    // #[case("123123", true)]
-    // #[case("222222", true)]
-    // #[case("123123123", false)]
-    // fn test_check_number_repetitions(#[case] value: &str, #[case] expected: bool) {
-    //     assert_eq!(check_number_repetitions(value), expected);
-    // }
+    #[rstest]
+    #[case("11", true)]
+    #[case("12", false)]
+    #[case("13", false)]
+    #[case("14", false)]
+    #[case("15", false)]
+    #[case("16", false)]
+    #[case("22", true)]
+    #[case("1010", true)]
+    #[case("123123", true)]
+    #[case("222222", true)]
+    #[case("123123123", false)]
+    fn test_check_number_repetitions(#[case] value: &str, #[case] expected: bool) {
+        assert_eq!(check_number_repetitions(value), expected);
+    }
 
     #[rstest]
     #[case(
