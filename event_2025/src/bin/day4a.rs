a5c6d5bde3a538d875b6a30e41953bc4ab42027e
= <jhowell45@googlemail.com>
2025-12-06 16:53:05 +0000

    adding tests and range checks.

--- event_2025/src/bin/day4a.rs
+++ event_2025/src/bin/day4a.rs
@@ -1,11 +1,12 @@
 use aoc_core::file_reader::get_file_contents;
 
+#[derive(Debug)]
 enum GridMarker {
     Empty,
     Paper,
-    Reachable,
 }
 
+#[derive(Debug)]
 pub struct Grid {
     map: Vec<GridMarker>,
     pub x_size: usize,
@@ -14,18 +15,19 @@ pub struct Grid {
 impl Grid {
     pub fn from_str(map: &str) -> Self {
         Self {
+            x_size: map.lines().nth(1).unwrap().chars().count(),
+            y_size: map.lines().collect::<Vec<&str>>().len(),
             map: map
                 .trim()
                 .chars()
+                .filter(|c| *c != '\n')
                 .into_iter()
                 .map(|c| match c {
                     '.' => GridMarker::Empty,
                     '@' => GridMarker::Paper,
-                    _ => panic!("Invalid map marker!"),
+                    _ => panic!("Invalid map marker '{}'!", c),
                 })
                 .collect(),
-            x_size: map.lines().nth(1).iter().len(),
-            y_size: map.lines().collect::<Vec<&str>>().len(),
         }
     }
 
@@ -34,14 +36,38 @@ impl Grid {
             .iter()
             .enumerate()
             .map(|(idx, m)| match m {
-                GridMarker::Paper | &GridMarker::Reachable => self.check_marker(idx) as u32,
-                GridMarker::Empty => 0
+                GridMarker::Paper => self.can_access_marker(idx) as u32,
+                GridMarker::Empty => 0,
             })
             .sum()
     }
 
-    fn check_marker(&self, index: usize) -> bool {
-        false
+    fn can_access_marker(&self, index: usize) -> bool {
+        let mut neighbours: usize = 0;
+        let left_check: bool = index > 0 && (index + 1) % self.x_size != 0;
+        let top_check: bool = index >= self.x_size;
+        let bottom_check: bool = index < (self.x_size * (self.y_size - 1));
+
+        if left_check {
+            neighbours += match self.map[index - 1] {
+                GridMarker::Paper => 1,
+                _ => 0,
+            }
+        }
+        if top_check {
+            neighbours += match self.map[index - self.x_size] {
+                GridMarker::Paper => 1,
+                _ => 0,
+            }
+        }
+        if bottom_check {
+            neighbours += match self.map[index + self.x_size] {
+                GridMarker::Paper => 1,
+                _ => 0,
+            }
+        }
+        println!("Index: {} || Neighbours: {}", index, neighbours);
+        neighbours < 4
     }
 }
 
@@ -53,5 +79,41 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    fn example() {}
+    #[case("..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.", (10, 10))]
+    #[case("..
+..
+..", (2, 3))]
+    fn test_grid_size(#[case] input: &str, #[case] expected: (usize, usize)) {
+        let grid: Grid = Grid::from_str(input);
+        assert_eq!((grid.x_size, grid.y_size), expected);
+    }
+
+    #[rstest]
+    #[case(
+        "..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.",
+        13
+    )]
+    fn example(#[case] paper_grid: &str, #[case] expected: u32) {
+        let grid: Grid = Grid::from_str(paper_grid);
+        println!("{:?}", grid);
+        assert_eq!(Grid::from_str(paper_grid).accessible_rolls(), expected);
+    }
 }
