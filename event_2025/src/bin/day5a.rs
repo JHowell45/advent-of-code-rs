37b3de1c481f1ac6c4e7b762adea13a4d18750be
= <jhowell45@googlemail.com>
2025-12-07 00:25:40 +0000

    more.

--- event_2025/src/bin/day5a.rs
+++ event_2025/src/bin/day5a.rs
@@ -3,22 +3,25 @@ use aoc_core::file_reader::get_file_contents;
 struct IdRange(u64, u64);
 
 struct FreshIngredientDB {
-    id_ranges: Vec<IdRange>
+    id_ranges: Vec<IdRange>,
 }
 
 impl FreshIngredientDB {
     fn from_str(ingredients_list: &str) -> Self {
         Self {
-            id_ranges: ingredients_list.lines().map(|line| {
-                let x: Vec<u64> = line.split("-").map(|v| v.parse::<u64>().unwrap()).collect();
-                IdRange(x[0], x[1])
-            }).collect::<Vec<IdRange>>()
+            id_ranges: ingredients_list
+                .lines()
+                .map(|line| {
+                    let x: Vec<u64> = line.split("-").map(|v| v.parse::<u64>().unwrap()).collect();
+                    IdRange(x[0], x[1])
+                })
+                .collect::<Vec<IdRange>>(),
         }
     }
 
     pub fn is_fresh(&self, id: u64) -> bool {
         for range in self.id_ranges.iter() {
-            if range.0 <= id &&  range.1 >= id {
+            if range.0 <= id && range.1 >= id {
                 return true;
             }
         }
@@ -29,14 +32,20 @@ impl FreshIngredientDB {
 fn fresh_ingredient_sum(input: String) -> u32 {
     let data = input.split("\n\n").collect::<Vec<&str>>();
     let fresh_db = FreshIngredientDB::from_str(data[0]);
-    data[1].lines().map(|l| {
-        let v: u64 = l.trim().parse().unwrap();
-        fresh_db.is_fresh(v) as u32
-    }).sum()
+    data[1]
+        .lines()
+        .map(|l| {
+            let v: u64 = l.trim().parse().unwrap();
+            fresh_db.is_fresh(v) as u32
+        })
+        .sum()
 }
 
 fn main() {
-    println!("The total number of fresh ingredients is: {}", fresh_ingredient_sum(get_file_contents(2025, 5)));
+    println!(
+        "The total number of fresh ingredients is: {}",
+        fresh_ingredient_sum(get_file_contents(2025, 5))
+    );
 }
 
 #[cfg(test)]
@@ -56,13 +65,14 @@ mod tests {
             "3-5
 10-14
 16-20
-12-18"
+12-18",
         );
         assert_eq!(fresh_db.is_fresh(check_id), expected);
     }
 
     #[rstest]
-    #[case("3-5
+    #[case(
+        "3-5
 10-14
 16-20
 12-18
@@ -72,7 +82,9 @@ mod tests {
 8
 11
 17
-32", 3)]
+32",
+        3
+    )]
     fn test_fresh_ingredient_sum(#[case] input: &str, #[case] expected: u32) {
         assert_eq!(fresh_ingredient_sum(String::from(input)), expected);
     }
--- event_2025/src/bin/day5b.rs
+++ event_2025/src/bin/day5b.rs
@@ -5,19 +5,26 @@ struct FreshRange(u64, u64);
 fn total_fresh_ids(ranges: &str) -> u64 {
     let parsed_ranges: Vec<FreshRange> = Vec::new();
     for range in ranges.lines() {
-        let parsed_range: Vec<u64> = range.split("-").map(|v| v.parse::<u64>().unwrap()).collect();
+        let parsed_range: Vec<u64> = range
+            .split("-")
+            .map(|v| v.parse::<u64>().unwrap())
+            .collect();
+        let fresh_range = FreshRange(parsed_range[0], parsed_range[1]);
     }
     parsed_ranges.iter().map(|range| range.1 - range.0).sum()
 }
 
 fn main() {
-    println!("The total number of fresh IDs is: {}", total_fresh_ids(get_file_contents(2025, 5).as_str()));
+    println!(
+        "The total number of fresh IDs is: {}",
+        total_fresh_ids(get_file_contents(2025, 5).as_str())
+    );
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn example() {
         let data: &str = "3-5
